379589c3a8e3988c6531a0ee1db20d053ac40c26
Tito <titocopie1@gmail.com>
2023-02-25 11:21:21 -0400

    upgrade DB config3

--- data/database.js
+++ data/database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
+const connectionProtocol = 'mongodb+srv';
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
